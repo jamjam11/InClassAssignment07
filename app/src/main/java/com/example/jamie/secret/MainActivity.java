96cc9746550cdc021f862d48049b60c4d9ff0c40
jamjam11 <jmg2307@tc.columbia.edu>
2017-03-09 20:45:06 -0500

    save and reveal methods update complete

--- app/src/main/java/com/example/jamie/secret/MainActivity.java
+++ app/src/main/java/com/example/jamie/secret/MainActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,18 +18,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void save(View view) {
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("secret");
+      FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("secret");
 
-        EditText editText = (EditText)findViewById(R.id.editText);
-        String userSecret = editText.getText().toString();
+     EditText user = (EditText) findViewById(R.id.name);
+       String username= user.getText().toString();
 
+        DatabaseReference myRef = database.getReference(username).child("secret");
+        EditText editText = (EditText) findViewById(R.id.userSecret);
+        String userSecret= editText.getText().toString();
+
+        editText.setText("");
+        user.setText("");
+
+        Toast t =Toast.makeText(this, "saved",Toast.LENGTH_SHORT);
+        t.show();
         myRef.setValue(userSecret);
     }
 
-    public void reveal(View view) {
-        Intent intent = new Intent (this, SecondActivity.class);
+    public void reveal(View view) {Intent intent = new Intent(this, SecondActivity.class);
+
+        EditText target =(EditText)findViewById(R.id.target);
+        intent.putExtra("target",target.getText().toString());
         startActivity(intent);
-    }
+        }
+
 }
--- app/src/main/java/com/example/jamie/secret/SecondActivity.java
+++ app/src/main/java/com/example/jamie/secret/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.jamie.secret;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -14,30 +15,40 @@ public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+     super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_second);
+
+        Intent intent =getIntent();
+        String target = intent.getStringExtra("target");
+
+
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("secret");
+        DatabaseReference myRef = database.getReference(target).child("secret");
 
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+       myRef.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
+               String value = dataSnapshot.getValue(String.class);
+                TextView textview = (TextView)findViewById(R.id.secret_display);
+                if(value!=null){
+                    textview.setText(value);
+                    }
+                else{
+                    textview.setText(getResources().getString(R.string.error_msg));
+                    }
 
-                TextView textView = (TextView) findViewById(R.id.secret_display);
-                textView.setText(value);
+                }
 
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
+           @Override
+           public void onCancelled(DatabaseError error) {
                 // Failed to read value
 
-            }
-        });
+           }
+       });
     }
+
+
 }
